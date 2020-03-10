d7e0e091d8097403f1856747dbe6cc07a964f3db
Marc Le Masson <SylarAtomic@gmail.com>
2020-03-10 13:58:55 +0200

    Completed Lessson 213 - 216 - Section Completed
    Lesson 213 - 216 completed
    + Get incredients from server db
    + removed old Interceptors
    + error handling
    ++ Realigned code with lecturer

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import Aux from "../Aux_Folder/Aux_File";
+import Aux from "../../hoc/Aux_Folder/Aux_File";
 import classes from "./Layout.css";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
