da97932b827ae9fc5de3c38105ab6642321d8639
Marc Le Masson <sylaratomic@gmail.com>
2021-02-28 22:54:37 +0000

    Revert "Completed Lesson 241 - Handling Form Submission"
    This reverts commit 6f5c3c0c8355079ddecac6aa1d4111615a84a314.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -66,15 +66,9 @@ class ContactData extends Component {
   orderHandler = (event) => {
     event.preventDefault();
     this.setState({ loading: true });
-    const formData = {};
-    for(let formElementIdentifier in this.state.orderForm) {
-      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
-
-    }
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
-      orderData: formData
     };
     axios
       .post("/orders.json", order)
@@ -107,7 +101,7 @@ class ContactData extends Component {
       });
     }
     let form = (
-      <form onSubmit={this.orderHandler}>
+      <form>
         <Input elementType="..." elementConfig="..." value="..." />
         {formElementsArray.map(formElement =>  (<Input 
         key={formElement.id}
